70f78396c6c764aaf3a7af96a99a8990c572580e
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2021-09-29 09:31:54 +0530

    Update browserstack.sh

--- browserstack.sh
+++ browserstack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 curl -u "$BrowserStack" \
 -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
--F "file=/var/lib/jenkins/workspace/browserstack-test/*.ipa"
+-F "file=@/var/lib/jenkins/workspace/browserstack-test/*.ipa"
