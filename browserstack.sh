757a95d7f560490dbc7239d4a97cf2e51ecb768d
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2021-09-29 11:18:44 +0530

    Update browserstack.sh

--- browserstack.sh
+++ browserstack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -u "$BrowserStack" \
 -X POST "https://api-cloud.browserstack.com/app-automate/upload" \
--F file=@"/var/lib/jenkins/workspace/browserstack-test/build_*.ipa" \
+-F file=$"/var/lib/jenkins/workspace/browserstack-test/build_*.ipa" \
 -F "custom_id=Lakeshore"
