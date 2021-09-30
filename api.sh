845f3ae552bee46d1afd2442e76a44807dc30b21
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2021-09-30 11:13:24 +0530

    Update api.sh

--- api.sh
+++ api.sh
@@ -2,4 +2,4 @@
 #sleep 25m
 LatestbuildNumber=$(curl -X GET "https://api.appcenter.ms/v0.1/apps/akash.raut/demo/branches/master/builds" -H  "accept: application/json" -H  "X-API-Token: $Appcenter" |jq -r '.[0].buildNumber') 
 echo $LatestbuildNumber
-appcenter build download --app akash.raut/demo --type build --id "16"
+appcenter build download --app akash.raut/demo --type build --id "$LatestbuildNumber"
