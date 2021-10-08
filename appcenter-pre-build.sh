de1bca98e6b871f0af575589b6aa43f1e8470a5b
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2021-10-08 20:09:39 +0530

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,6 +3,6 @@
    # latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
   #  echo $latestTag
     chmod a+x /Users/runner/work/1/s/ios/ReactNativeStarter/Info.plist
-    plutil -replace CFBundleVersion -string "2" 
+    usr/libexec/PlistBuddy -replace CFBundleVersion -string "2" 
     $APPCENTER_SOURCE_DIRECTORY/ios/ReactNativeStarter/Info.plist
 
