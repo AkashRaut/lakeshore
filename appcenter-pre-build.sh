4a2477d89987053c25273128de6d84f27d4a26a7
AkashRaut <32610548+AkashRaut@users.noreply.github.com>
2021-10-08 20:55:47 +0530

    Update appcenter-pre-build.sh

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,6 +2,7 @@
 
     latestTag=$(git describe --tags $(git rev-list --tags --max-count=1))
     echo $latestTag
+    brew install brew gnu-sed
     chmod a+x $APPCENTER_SOURCE_DIRECTORY/ios/ReactNativeStarter/Info.plist
     sed -i "${20}s/1.0/"$latestTag"-qa/" 
     $APPCENTER_SOURCE_DIRECTORY/ios/ReactNativeStarter/Info.plist
